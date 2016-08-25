81e8dacff2d484cf9cb0f6124d9711e408c567c9
Sam Cooper <sacooper@pivotal.io>
2016-08-25 11:19:29 +0100

    Install cf CLI before running smoke tests
    Signed-off-by: Jacek Rzeniewicz <jrzeniewicz@pivotal.io>

--- scripts/run-smoke-tests.sh
+++ scripts/run-smoke-tests.sh
@@ -5,6 +5,14 @@ set -e
 export CONFIG=$(find `pwd`/cf-smoke-tests-config -name *.json)
 echo "Using CONFIG=$CONFIG"
 
+
+apt update
+apt install -y curl
+curl -L 'https://cli.run.pivotal.io/stable?release=linux64-binary&version=6.21.1' | tar -zx -C /usr/local/bin
+
+
+
+
 pushd cf-smoke-tests 
   ./bin/test
 popd
