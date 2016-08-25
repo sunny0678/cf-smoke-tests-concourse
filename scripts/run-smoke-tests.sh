3976e8c023f1bb7d78e8986e1904c4523f217a50
Jacek Rzeniewicz <jrzeniewicz@pivotal.io>
2016-08-25 10:47:38 +0100

    Clean up script
    Signed-off-by: Sam Cooper <sacooper@pivotal.io>

--- scripts/run-smoke-tests.sh
+++ scripts/run-smoke-tests.sh
@@ -5,11 +5,6 @@ set -e
 export CONFIG=$(find `pwd`/cf-smoke-tests-config -name *.json)
 echo "Using CONFIG=$CONFIG"
 
-echo "GOPATH: $GOPATH"
 pushd cf-smoke-tests 
-  # go get ./...
   ./bin/test
 popd
-
-
-echo "done"
