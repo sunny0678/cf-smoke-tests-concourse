344497d75a102fee440788e2896d8fcf80fa340d
marcus-samsungsds <atplus12345@gmail.com>
2017-10-06 14:47:05 +0900

    Update run-smoke-tests.sh

--- scripts/run-smoke-tests.sh
+++ scripts/run-smoke-tests.sh
@@ -2,9 +2,35 @@
 
 set -e
 
-export CONFIG=cf-credential/$ENV-smoke-tests.json
+export CONFIG=smoke-tests.json
 echo "Using CONFIG=$CONFIG"
 
+
+cat <<EOF > $CONFIG
+{
+  "suite_name"                      : "CF_SMOKE_TESTS",
+  "api"                             : "api.${system-domain}",
+  "apps_domain"                     : "app.${system-domain}",
+  "user"                            : "admin",
+  "password"                        : "${cf-admin-password}",
+  "org"                             : "CF-SMOKE-ORG",
+  "space"                           : "CF-SMOKE-SPACE",
+  "isolation_segment_space"         : "CF-SMOKE-ISOLATED-SPACE",
+  "cleanup"                         : true,
+  "use_existing_org"                : true,
+  "use_existing_space"              : true,
+  "logging_app"                     : "",
+  "runtime_app"                     : "",
+  "enable_windows_tests"            : false,
+  "windows_stack"                   : "windows2012R2",
+  "enable_etcd_cluster_check_tests" : false,
+  "etcd_ip_address"                 : "",
+  "backend"                         : "diego",
+  "isolation_segment_name"          : "is1",
+  "isolation_segment_domain"        : "is1.bosh-lite.com",
+  "enable_isolation_segment_tests"  : false
+}
+EOF
 
 # Downloading CF CLI.
 # This is a static dependency and really should be part of the Docker image.
