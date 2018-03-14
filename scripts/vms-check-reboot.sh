2d65d6e622068272eea6e5d2fcb56f811337636d
marcus-samsungsds <atplus12345@gmail.com>
2018-03-14 10:46:30 +0900

    Update vms-check-reboot.sh

--- scripts/vms-check-reboot.sh
+++ scripts/vms-check-reboot.sh
@@ -6,5 +6,8 @@ DOWN=`bosh vms -d ${DEP}|grep failing|wc -l`
 NODE=`bosh vms -d ${DEP}|grep failing|grep ${DEP_NODE}|awk '{print $1}'|head -1`
 
 if [ $DOWN -gt 0 ];then
+echo "=============reboot $DEP $NODE==============="
 bosh -d ${DEP} ssh $NODE -c "sudo reboot"
+sleep 120
+bosh vms -d ${DEP}
 fi
